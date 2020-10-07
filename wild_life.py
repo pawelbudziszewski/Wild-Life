75fc88c6860e43090a32450cc52776637b351040
Paweł Budziszewski <pawel@arkanian.pl>
2020-10-07 11:07:08 +0200

    Small update in formatting

--- wild_life.py
+++ wild_life.py
@@ -2,8 +2,7 @@
 An interactive version of Conway’s Life Game, written in Python. 
 
 Source and description:
-https://github.com/pawelbudziszewski/Wild-Life
-
+   https://github.com/pawelbudziszewski/Wild-Life
 
 Copyright 2020 Paweł Budziszewski
 
