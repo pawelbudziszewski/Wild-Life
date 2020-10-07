ff0bd4f09a83e4dadd8971ce99ffb92f2468283b
Paweł Budziszewski <pawel@arkanian.pl>
2020-10-07 11:06:04 +0200

    Update life_forms.py

--- life_forms.py
+++ life_forms.py
@@ -1,10 +1,29 @@
-"""Species to be used by Wild Life app.
+"""Wild Life - Species to be used by Wild Life app.
+
 Each life form should be provided as a list of strings (one string per
 line), with '#' character representing life cell, any other character
 representing background.
 All life forms must be converted to Numpy array of floats using 
 convert_species()
+
+Source and description:
+https://github.com/pawelbudziszewski/Wild-Life
+
+Copyright 2020 Paweł Budziszewski
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+   
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
+
 import numpy as np
 
 def convert_species(species):
