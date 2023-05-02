c79edd6c55b8936c814c5d6837a492260e44a03f
Paweł Budziszewski <pawel.budziszewski@robotec.ai>
2023-05-02 22:17:29 +0200

    Change np.random.rand to np.eye
    Plus small code reorganization - code was reduced to 141 bytes (from 153)

--- minimal_code/life_minimalistic.py
+++ minimal_code/life_minimalistic.py
@@ -1,2 +1,2 @@
-from cv2 import*;from numpy import*;W=random.rand(800,999)<.8
-while waitKey(1)<0:N=filter2D(W*1.,6,ones((3,3)))-W;W=(N==3)|(W==1)&(N==2);imshow('',W*1.)
+from cv2 import*;from numpy import*;W=eye(800,999)
+while waitKey(1)<0:N=filter2D(W,6,ones((3,3)))-W;W=((N==3)|(W==1)&(N==2))*1.;imshow('',W)
